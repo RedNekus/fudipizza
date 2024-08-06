7fd6e4b4291ed4fe485eedf7eff12b6cd2bdc042
RedNekus <fighterneko@gmail.com>
2024-08-06 18:25:11 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
+
+function buildStyles() {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+};
+
+function buildHTML() {
+    return gulp.src('./src/**/*.pug')
+        .pipe(pug({pretty: true}))
+        .pipe(gulp.dest('.'));
+};
+
+
+exports.buildStyles = buildStyles;
+exports.buildHTML = buildHTML;
+exports.watch = function () {
+    gulp.watch('./sass/**/*.scss',  gulp.series('buildStyles'));
+    gulp.watch('./src/**/*.pug',  gulp.series('buildHTML'));
+}
