02fb4900c26bd945074f3440b948d6568c4075f7
RedNekus <fighterneko@gmail.com>
2024-08-07 10:39:19 +0300

    07.08 - add img

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,21 @@ const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
 
 function buildStyles() {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 };
 
 function buildHTML() {
-    return gulp.src('./src/**/*.pug')
+    return gulp.src('./src/*.pug')
         .pipe(pug({pretty: true}))
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('./'));
 };
 
 
 exports.buildStyles = buildStyles;
 exports.buildHTML = buildHTML;
 exports.watch = function () {
-    gulp.watch('./sass/**/*.scss',  gulp.series('buildStyles'));
+    gulp.watch('./src/sass/**/*.scss',  gulp.series('buildStyles'));
     gulp.watch('./src/**/*.pug',  gulp.series('buildHTML'));
 }
