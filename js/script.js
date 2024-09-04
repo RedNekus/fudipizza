cede303a0be6c77f16ee8d0ca9de5c4487a1e0a1
RedNekus <fighterneko@gmail.com>
2024-09-04 16:58:14 +0300

    04.09

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-console.log('111')
+document.addEventListener("DOMContentLoaded", () => {
+    const slider = document.querySelector(`[JS_Slider]`)
+    let blockDots = document.querySelector(`.jobs__dots`)
+    let dots = blockDots.children;
+    let items = slider.children
+
+    const updateParentHeight = () => {
+        if(items[0]) {
+            let position = items[0].getBoundingClientRect();
+            slider.style.height = `${position.height}px`;
+        }
+    }
+    window.onload = updateParentHeight
+    window.addEventListener('resize', updateParentHeight)
+    //let mid = parseInt(items.length/2)
+    items[1].classList.add(`active`)
+    dots[1].classList.add(`active`)
+    if(null !== dots && dots.length > 0) {
+        for (let itemDot of dots) {
+            itemDot.addEventListener('click', () => {
+                if(!itemDot.classList.contains('active')) {
+                    for (let item of dots) {
+                        if(item.classList.contains('active')) {
+                            item.classList.remove('active')
+                        }
+                    }
+                    itemDot.classList.add('active');
+                    let index = itemDot.dataset.num
+                    if(null !== index && items[index]) {
+                        //items[index].classList.add(`is-hidden`)
+                    }
+                }
+            })
+        }
+    }
+})
